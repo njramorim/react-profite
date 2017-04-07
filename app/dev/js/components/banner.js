30992dcc337b98ffe2de0ba40b67e00aefe33557
Nelson PC <njr.amorim@gmail.com>
2017-04-07 20:57:17 -0300

    responsivo

--- app/dev/js/components/banner.js
+++ app/dev/js/components/banner.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Banner = (props) => {
 	return <figure className={`banner  ${props.active}`}
 		name={props.name} 
-		data-mobile={props.imageMobile}
 	>
 		<a href={props.url}>
 			<img src={props.image} />
+			<img src={props.imageMobile} />
 			<figcaption>{props.text}</figcaption>
 		</a>
 	</figure>
